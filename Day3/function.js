2674cf8bbf1c695ffcf70bf2120a559f0dd2c5e3
Muhammad361738 <ullahehsan016@gmail.com>
2024-08-16 00:19:50 +0500

    function

---
+++ Day3/function.js
@@ -0,0 +1,14 @@
+// Function 
+//  syntax
+function myProblem (){
+    console.logf(`To Learn English `)
+}
+myProblem();
+// Three type to initilize 
+// 1) function 
+// 2) Anoymoyous function which have only function key word
+// 3) Arraw function 
+let rectangle = function (height, width){ // parameter 
+    return height * width
+}
+console.log(rectangle(7,7)) // Argument 
