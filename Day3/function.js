f082e290b40be04f9f1818788e52cdd06dcbdb70
Muhammad361738 <ullahehsan016@gmail.com>
2024-08-16 01:04:05 +0500

    Expresss Function types

--- Day3/function.js
+++ Day3/function.js
@@ -1,14 +1,48 @@
-// Function 
-//  syntax
-function myProblem (){
-    console.logf(`To Learn English `)
+// // Function 
+// //  syntax
+// function myProblem (){
+//     console.log(`To Learn English `)
+// }
+// myProblem();
+// // Expression
+
+// // 1) Named function 
+// let myFunction = function name (name){
+//     console.log(`my name is is ${name}`)
+// }
+// myFunction("Talha");
+// // 2) Anoymoyous function which have only function key word
+// let rectangle = function (height, width){ // parameter 
+//     return height * width
+// }
+// console.log(rectangle(7,7)) // Argument 
+// // 3) Arraw function 
+// const Arrowfun = (num) => {return num*num}
+// console.log(Arrowfun(6));
+// //  Neasted function 
+// function outer (name1){
+//     console.log(`hello world ${name1}`);
+//     function inner (name){
+//         console.log(`hello function ${name} `);
+//     }
+//     inner ("zain");
+
+// }
+// outer("Talha");
+
+// musth example 
+
+
+function vowel (str){
+    count = 0 ;
+    let char = "aeiouAEIOU"
+
+    for (let vowels of str){
+      
+        if (char.includes(vowels) ){
+        count ++
+        }
+     }
+    return count 
 }
-myProblem();
-// Three type to initilize 
-// 1) function 
-// 2) Anoymoyous function which have only function key word
-// 3) Arraw function 
-let rectangle = function (height, width){ // parameter 
-    return height * width
-}
-console.log(rectangle(7,7)) // Argument 
+console.log(vowel("Talhaaaaaa"))
