dd7a972a6939cd7e0616e7712041f91f37f0c16a
Muhammad361738 <ullahehsan016@gmail.com>
2024-08-17 00:57:37 +0500

    Date

---
+++ Day4/Date&Time.js
@@ -0,0 +1,14 @@
+//  date 
+let date = new Date ()
+console.log(date);
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toLocaleString());
+console.log(date.toLocaleDateString());
+console.log(date.toTimeString())
+
+
+let myDate = new Date("2004 4 11 ");
+console.log(myDate)
+// get method
+// set method 
