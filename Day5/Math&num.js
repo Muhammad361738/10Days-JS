9cbda0d1cdddd757545a41b1bcf3a7dc0e563525
Muhammad361738 <ullahehsan016@gmail.com>
2024-08-19 00:39:46 +0500

    Math & Number

---
+++ Day5/Math&num.js
@@ -0,0 +1,16 @@
+// Number ****************************
+// const num = new Number (50000000000);
+// console.log(num);
+// console.log(num.toLocaleString())
+// console.log(num.toString())
+// console.log(num.toFixed())
+// console.log(num.toLocaleString())
+//  Math
+console.log(Math.round(25.456))
+console.log(Math.ceil(23.755))
+console.log(Math.floor(67.98))
+console.log(Math.min(25,54,40,67,76,83))
+console.log(Math.max(25,54,40,67,76,83))
+const newNum = Math.floor(Math.random ()* 10) 
+
+console.log(newNum)
