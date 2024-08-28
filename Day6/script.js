60bcd3f16963247ef28f02de9adec4a4e039553b
Muhammad361738 <ullahehsan016@gmail.com>
2024-08-29 03:15:01 +0500

    Introduction of DoM & Bom

---
+++ Day6/script.js
@@ -0,0 +1,28 @@
+// Dom is Document object model and Bom is Browser object Model
+
+/*
+Element node 
+Attribute node 
+Comment node 
+documenty node 
+
+*/
+
+// Accessability 
+/*
+getElementById()
+getElementByClass()
+getElementByTagName()
+querryselector()
+querrySelectorAll()
+
+*/
+    
+let Name=document.getElementById("myName")
+console.log(Name);
+
+const det = document.getElementsByClassName("dtail")
+console.log(det);
+
+let myDet = document.querySelector("ul")
+console.log(myDet)
