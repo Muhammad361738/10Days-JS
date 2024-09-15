d22cbc37c6b05fb3c6c7fc4ee84c775af422d057
Muhamma361738 <ullahehsan016@gmail.com>
2024-09-15 22:43:17 +0500

    explore some dom

--- Day6/script.js
+++ Day6/script.js
@@ -18,11 +18,21 @@ querrySelectorAll()
 
 */
     
-let Name=document.getElementById("myName")
-console.log(Name);
+// let Name=document.getElementById("myName")
+// console.log(Name);
 
-const det = document.getElementsByClassName("dtail")
-console.log(det);
+// const det = document.getElementsByClassName("dtail")
+// console.log(det);
 
-let myDet = document.querySelector("ul")
-console.log(myDet)
+// let myDet = document.querySelector("ul")
+// console.log(myDet)
+// introduction od dom 
+const details = document.querySelector(".hide")
+
+const btn = document.querySelector(".show")
+
+btn.addEventListener("click", ()=>{
+    details.textContent
+    console.log(details)
+   console.log("clicked");
+})
